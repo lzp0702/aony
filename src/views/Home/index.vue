6b997447d99bdc6e94c17f7d45ec9e0eb759808f
lzp0702 <54069122+lzp0702@users.noreply.github.com>
2020-05-21 21:13:45 +0800

    路由基础搭建

---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='page-home'>
+    <h1 @click="goLogin">首页</h1>
+    <router-link to='classify'>{{string}}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      string: '李志鹏'
+    }
+  },
+  methods: {
+    goLogin () {
+      this.$router.push('./classify')
+    }
+  }
+}
+</script>
