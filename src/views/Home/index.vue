2e569a1956fe6e85e6c5af52052d7a38217559ae
lzp0702 <54069122+lzp0702@users.noreply.github.com>
2020-05-22 02:57:57 +0800

    限制540大小

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,22 +1,58 @@
 <template>
   <div class='page-home'>
-    <h1 @click="goLogin">首页</h1>
-    <router-link to='classify'>{{string}}</router-link>
+    <header class="index-header">
+      <a href="mine.html?cpid=0">
+        <div class="header-user">
+            <div class="user-btn"></div>
+
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="search.html?cpid=0">
+        <div class="header-search"></div>
+      </a>
+    </header>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data () {
-    return {
-      string: '李志鹏'
-    }
-  },
-  methods: {
-    goLogin () {
-      this.$router.push('./classify')
-    }
-  }
+  name: 'home'
 }
 </script>
+
+<style lang="scss" scoped>
+.page-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.index-header{
+  display: flex;
+  height: 44px;
+  justify-content:space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  .user-btn{
+    width: 25px;
+    height: 25px;
+    background: url('../../assets/icon/user-btn.png') no-repeat;
+    background-size: 100%;
+  }
+  .header-logo{
+    width: 92px;
+    height: 28px;
+    background: url('../../assets/icon/header-logo.png') no-repeat;
+    background-size: 100%;
+  }
+  .header-search{
+    width: 25px;
+    height: 25px;
+    background: url('../../assets/icon/header-search.png') no-repeat;
+    background-size: 100%;
+  }
+}
+
+</style>
